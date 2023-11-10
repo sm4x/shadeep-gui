5bd0107a5cbe5a4c38930515e53a6b2b9acd90c7
sm4x <max.schattauer@gmail.com>
2023-11-10 21:14:36 +0100

    Update main.py
    Treeview update

--- main.py
+++ main.py
@@ -4,65 +4,79 @@ import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.font as font
 
-SHASUMS = "sha256sums.txt"
-current_dir=os.getcwd()
 
 class Application(tk.Tk):                                       # Application is a Tk Object
     def __init__(self) -> None:                                 
         super().__init__()                                      # Application inherits from Tk
         self.title("SHAdeep")                                   # Application Title
-        #self.geometry('1200x700')                               # root window size
+        self.geometry('1200x700')                               # root window size
         self.interface()                                        # Create Interface as ttk object
-        #self.build_tree()
-        
+  
         
     def interface(self):
     #### Main Frame as TTK object##############################################################
-        mf = ttk.Frame(self)
-        frame = ttk.Frame(mf, borderwidth=5, relief="ridge", width=200, height=100)
-        """self.tree  = ttk.Treeview(frame, 
-                                      columns=self.tree_columns, 
-                                      show="headings")"""
+        tree_columns = ("country", "capital", "currency")
+        tree_data = [
+            ("Argentina",      "Buenos Aires",     "ARS"),
+            ("Australia",      "Canberra",         "AUD"),
+            ("Brazil",         "Brazilia",         "BRL"),
+            ("Canada",         "Ottawa",           "CAD"),
+            ("China",          "Beijing",          "CNY"),
+            ("France",         "Paris",            "EUR"),
+            ("Germany",        "Berlin",           "EUR"),
+            ("India",          "New Delhi",        "INR"),
+            ("Italy",          "Rome",             "EUR"),
+            ("Japan",          "Tokyo",            "JPY"),
+            ("Mexico",         "Mexico City",      "MXN"),
+            ("Russia",         "Moscow",           "RUB"),
+            ("South Africa",   "Pretoria",         "ZAR"),
+            ("United Kingdom", "London",           "GBP"),
+            ("United States",  "Washington, D.C.", "USD")
+            ]
+    
+        main_frame = ttk.Frame(self)
+        tree_frame = ttk.Frame(main_frame, borderwidth=5, relief="ridge", width=200, height=100)
+        self.tree_view = ttk.Treeview(tree_frame,
+                                      columns=tree_columns,
+                                      show="headings")
+        vsb = ttk.Scrollbar(tree_frame, orient="vertical", command=self.tree_view.yview)
+        hsb = ttk.Scrollbar(tree_frame, orient="horizontal", command=self.tree_view.xview)
+        self.tree_view.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
         
-        
-        
-        namelbl = ttk.Label(mf, text="Name")
-        name = ttk.Entry(mf)
 
-        one = ttk.Checkbutton(mf, text="One", onvalue=True)
-        two = ttk.Checkbutton(mf, text="Two", onvalue=True)
-        three = ttk.Checkbutton(mf, text="Three", onvalue=True)
-        ok = ttk.Button(mf, text="Okay")
-        cancel = ttk.Button(mf, text="Cancel")
-
-        mf.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
-        frame.grid(column=0, row=0, columnspan=3, rowspan=2, sticky=(tk.N, tk.S, tk.E, tk.W))
-        namelbl.grid(column=3, row=0, columnspan=2, sticky=(tk.N, tk.W), padx=5)
-        name.grid(column=3, row=1, columnspan=2, sticky=(tk.N,tk.E,tk.W), pady=5, padx=5)
-        one.grid(column=0, row=3)
-        two.grid(column=1, row=3)
-        three.grid(column=2, row=3)
-        ok.grid(column=3, row=3)
-        cancel.grid(column=4, row=3)
+        main_frame.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
+        tree_frame.grid(column=0, row=0, columnspan=3, rowspan=2, sticky=(tk.N, tk.S, tk.E, tk.W))
+        self.tree_view.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
+        vsb.grid(column=1, row=0, sticky=(tk.N, tk.S))
+        hsb.grid(column=0, row=1, sticky=(tk.E, tk.W))
 
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
-        mf.columnconfigure(0, weight=3)
-        mf.columnconfigure(1, weight=3)
-        mf.columnconfigure(2, weight=3)
-        mf.columnconfigure(3, weight=1)
-        mf.columnconfigure(4, weight=1)
-        mf.rowconfigure(1, weight=1)
+        main_frame.columnconfigure(0, weight=1)
+        main_frame.rowconfigure(1, weight=1)
+        tree_frame.columnconfigure(0, weight=1)
+        tree_frame.rowconfigure(0, weight=1)
     
+    
+    def sortby(tree, col, descending):
+        """Sort tree contents when a column is clicked on."""
+        # grab values to sort
+        data = [(tree.set(child, col), child) for child in tree.get_children('')]
 
+        # reorder data
+        data.sort(reverse=descending)
+        for indx, item in enumerate(data):
+            tree.move(item[1], '', indx)
+
+        # switch the heading so that it will sort in the opposite direction
+        tree.heading(col,
+            command=lambda col=col: sortby(tree, col, int(not descending)))
     
 ####################
 # Main Application #
 ####################
 
 if __name__ == "__main__":      # if run from main program
-  
-  
     app = Application()           # Create App Object
     app.mainloop()                # Run mainloop
  
